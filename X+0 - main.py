c531ef00107a9cb2fbd73a807a17cc1bb9b6494d
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2020-10-06 09:42:41 +0100

    Update X+0 - main.py

--- X+0 - main.py
+++ X+0 - main.py
@@ -26,7 +26,7 @@ while True:
     print("-------------------")
     print("press enter to begin")
 
-    input()
+    gameType = int(input())
 
     print("\n do you want to play singleplayer or multiplayer \n s = singleplayer \n m = multiplayer")
     gameType = ""
@@ -46,7 +46,8 @@ while True:
         if singlePlayerType != "x" and singlePlayerType != "o":
             singlePlayerType = "x"
         while gameRunning == True:
-            # player turn
+            #player turn
+            #figure out n number
             if singlePlayerType == "x":
                 singlePlayerRedoTurn = True
                 while singlePlayerRedoTurn == True:
@@ -72,6 +73,7 @@ while True:
                             print("Error, expected 1, 2, or 3 \n please re-enter number")
                         elif singlePlayerColumnSelect == 1 or singlePlayerColumnSelect == 2 or singlePlayerColumnSelect == 3:
                             break
+                            
                     if singlePlayerRowSelect == 1 and singlePlayerColumnSelect == 1:
                         if row1A == "X" or row1A == "O":
                             singlePlayerRedoTurn = True
@@ -143,11 +145,65 @@ while True:
                         elif row3C != "X" and row3C != "O":
                             row3C = "X"
                             singlePlayerRedoTurn = False
+                            
+                            
+            #test for player win
+            
+            print("checking win conditions")
+            if row1A == "X" and row1B == "X" and row1C == "X":
+                print("player won")
+                #end
+            if row2A == "X" and row2B == "X" and row2C == "X":
+                print("player won")
+                #end
+            if row3A == "X" and row3B == "X" and row3C == "X":
+                print("player won")
+                #end
+            if row1A == "X" and row2A == "X" and row3A == "X":
+                print("player won")
+                #end
+            if row1B == "X" and row2B == "X" and row3B == "X":
+                print("player won")
+                #end
+            if row1C == "X" and row2C == "X" and row3C == "X":
+                print("player won")
+                #end
+             if row1A == "X" and row2B == "X" and row3C == "X":
+                print("player won")
+                #end
+            if row3A == "X" and row2B == "X" and row1C == "X":
+                print("player won")
+                #end
+          
+              
 
 
             #Computer Turn
+            time.sleep(2)
             print("computer turn here")
-
+            if row1A == "X" and row1B == "X" and row1C != "X":
+                row1C = "O"
+            if row1B == "X" and row1C == "X" and row1A != "X":
+                row1A = "O"
+            if row1C == "X" and row1A == "X" and row1B != "X":
+                row1B = "O"
+                
+            if row2A == "X" and row2B == "X" and row2C != "X":
+                row2C = "O"
+            if row2B == "X" and row2C == "X" and row2A != "X":
+                row2A = "O"
+            if row2C == "X" and row2A == "X" and row2B != "X":
+                row2B = "O"
+               
+            if row3A == "X" and row3B == "X" and row3C != "X":
+                row3C = "O"
+            if row3B == "X" and row3C == "X" and row3A != "X":
+                row2A = "O"
+            if row3C == "X" and row3A == "X" and row3B != "X":
+                row3B = "O"
+                
+            
+                
 
 
 
